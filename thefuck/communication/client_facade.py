68f4c42aa02fb5d800dad6d0ae43961f315b930c
jeinglis <jeinglis@ucalgary.ca>
2016-04-10 19:21:24 -0600

    fixign imports

--- thefuck/communication/client_facade.py
+++ thefuck/communication/client_facade.py
@@ -1,7 +1,7 @@
-from thefuck.corrector import get_corrected_commands
+import corrector
 from .sender import send_to_client
 
 
 def receive_command(command, connection_socket):
-    corrected_commands = get_corrected_commands(command)
+    corrected_commands = corrector.get_corrected_commands(command)
     send_to_client(corrected_commands, connection_socket)
--- thefuck/communication/receiver.py
+++ thefuck/communication/receiver.py
@@ -1,6 +1,6 @@
 from socket import *
-from .clientFacade import receive_command
-from .sender import send_to_client
+from . import client_facade
+from . import sender
 import pickle
 
 def listen_for_connection(port):
@@ -12,13 +12,13 @@ def listen_for_connection(port):
     while 1:
         connection_socket, addr = server_socket.accept()
         command = pickle.loads(connection_socket.recv(1024))
-        send_for_correction(command, connection_socket)
-        #send_back(command, connection_socket)
+        #send_for_correction(command, connection_socket)
+        send_back(command, connection_socket)
 
 
 def send_for_correction(command, connection_socket):
-    receive_command(command, connection_socket)
+    client_facade.receive_command(command, connection_socket)
 
 def send_back(command, connection_socket):
-    send_to_client(command, connection_socket)
+    sender.send_to_client(command, connection_socket)
 
