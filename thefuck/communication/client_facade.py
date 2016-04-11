16d3a3fb01596ca9b12f39b15e775a9a8ecdeb06
Enoch Tsang <etsang1@hotmail.com>
2016-04-10 21:21:55 -0600

    slight fix

--- thefuck/communication/client_facade.py
+++ thefuck/communication/client_facade.py
@@ -6,22 +6,20 @@ def receive_command(command, connection_socket):
     if corrected_commands == None:
         print("Sending none")
         send_to_client(None, connection_socket)
-        
-    print("Sending off")
-    packaged_corrected_commands = []
-    for command in corrected_commands:
-        print(command.script)
-        if command.side_effect == None:
-            packaged_corrected_commands.append((command.script, # script 
-                                              None, # side_effect
-                                              command.priority)) # priority
-
-        else:
-            packaged_corrected_commands.append((command.script, # script 
-                                                command.side_effect[0].script, # side_effect
-                                                command.side_effect[0].stdout,
-                                                command.side_effect[0].stderr,
-                                                command.side_effect[1],
-                                                command.priority)) # priority
-
-    send_to_client(packaged_corrected_commands, connection_socket)
+    else:        
+        print("Sending off")
+        packaged_corrected_commands = []
+        for command in corrected_commands:
+            print(command.script)
+            if command.side_effect == None:
+                packaged_corrected_commands.append((command.script, # script 
+                                                  None, # side_effect
+                                                  command.priority)) # priority
+            else:
+                packaged_corrected_commands.append((command.script, # script 
+                                                    command.side_effect[0].script, # side_effect
+                                                    command.side_effect[0].stdout,
+                                                    command.side_effect[0].stderr,
+                                                    command.side_effect[1],
+                                                    command.priority)) # priority
+        send_to_client(packaged_corrected_commands, connection_socket)
